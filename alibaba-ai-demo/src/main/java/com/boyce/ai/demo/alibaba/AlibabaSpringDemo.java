4adf9a7d81b22fed791af9df70055f547411fd09
Boyce <sloan6077@gmail.com>
2024-06-16 18:31:01 +0800

    【初始化】

---
+++ alibaba-ai-demo/src/main/java/com/boyce/ai/demo/alibaba/AlibabaSpringDemo.java
@@ -0,0 +1,17 @@
+package com.boyce.ai.demo.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author by zhaozhong
+ * @date 2024/6/16 15:58
+ */
+@SpringBootApplication
+public class AlibabaSpringDemo {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AlibabaSpringDemo.class);
+    }
+}
